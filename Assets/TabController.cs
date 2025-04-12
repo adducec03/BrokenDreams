9c4e821ce8cd91d138cbcdce025a1aecfe562537
Carmine Adduce <adducec@gmail.com>
2025-04-12 20:33:40 +0200

    aggiustato funzionamento menu e iniziato il layout di PlayerPage

--- Assets/TabController.cs
+++ Assets/TabController.cs
@@ -14,15 +14,16 @@ public class TabController : MonoBehaviour
         ActivateTab(0);
     }
 
+
     public void ActivateTab(int tabNo)
     {
         for (int i = 0; i < pages.Length; i++)
         {
             pages[i].SetActive(false);
-            tabImages[i].color = Color.black;
+            tabImages[i].color = Color.grey;
         }
         pages[tabNo].SetActive(true);
-        tabImages[tabNo].color = Color.red;
+        tabImages[tabNo].color = Color.white;
     }
 
 }
--- Assets/menuController.cs
+++ Assets/menuController.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class menuController : MonoBehaviour
 {
     public GameObject menuCanvas;
+    public Button menuButton; // Riferimento al pulsante del menu
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,5 +18,24 @@ public class menuController : MonoBehaviour
         {
             menuCanvas.SetActive(!menuCanvas.activeSelf);
         }
+    }
+
+    public void ToggleMenu()
+    {
+        // Inverte la visibilità del menu
+        bool isMenuActive = !menuCanvas.activeSelf;
+        menuCanvas.SetActive(isMenuActive);
+
+        Time.timeScale = 0f;    // Pausa il gioco quando il menu è attivo, ripristina la velocità del gioco quando il menu è nascosto
+
+        // Disabilita il pulsante quando il menu è attivo, riabilitalo quando il menu è nascosto
+        menuButton.gameObject.SetActive(!isMenuActive); // Nasconde il pulsante quando il menu è aperto
+    }
+
+    public void ReturnToGame()
+    {
+        menuCanvas.SetActive(false);  // Nasconde il menu
+        Time.timeScale = 1f;          // Ripristina la velocità del gioco (se il gioco era messo in pausa)
+        menuButton.gameObject.SetActive(true); // Mostra il pulsante del menu
     }
 }
