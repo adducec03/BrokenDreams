7731635d9de52ef9f2ef1e4f0258fcf1452d13fc
AntonioSouls <tonyplanza1@gmail.com>
2025-04-26 19:09:18 +0200

    Aggiunta schermata di selezione dei livelli (adesso portano temporaneamente tutti al livello 1. Rimane da modellare il comportamento che i livelli successivi si sbloccano solo una volta completati i livelli precedenti)

--- Assets/scripts/MainMenuController.cs
+++ Assets/scripts/MainMenuController.cs
@@ -5,23 +5,29 @@ public class MainMenuController : MonoBehaviour
 {
     public GameObject mainPanel;
     public GameObject settingsPanel;
+    public GameObject playPanel;
     
 
     public void Start()
     {
+        playPanel.SetActive(false);  //Assicurati che il pannello del tasto Play sia nascosto all'inizio
         settingsPanel.SetActive(false); // Assicurati che il pannello delle impostazioni sia nascosto all'inizio
         mainPanel.SetActive(true); // Assicurati che il pannello principale sia visibile all'inizio
     }
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("Level1");
+        settingsPanel.SetActive(false); // Nasconde il pannello delle impostazioni quando il playPanel è aperto
+        mainPanel.SetActive(false); // Nasconde il pannello principale quando il playPanel è aperto
+        playPanel.SetActive(true);
+        
     }
 
     public void OpenSettings()
     {
         settingsPanel.SetActive(true);
         mainPanel.SetActive(false); // Nasconde il pannello principale quando le impostazioni sono aperte
+        playPanel.SetActive(false); // Nasconde il pannello del tasto play quando le impostazioni sono aperte
     }
 
     public void QuitGame()
@@ -32,7 +38,28 @@ public class MainMenuController : MonoBehaviour
 
     public void CloseSettings()
     {
+        playPanel.SetActive(false);
         settingsPanel.SetActive(false);
         mainPanel.SetActive(true); // Mostra di nuovo il pannello principale quando le impostazioni sono chiuse
+    }
+
+    public void SelectLevelOne()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void SelectLevelTwo()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void SelectLevelThree()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void SelectLevelFour()
+    {
+        SceneManager.LoadScene("Level1");
     }
 }
