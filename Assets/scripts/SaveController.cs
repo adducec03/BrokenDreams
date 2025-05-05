047ac2227f11d3dc29fb84f3ab568e999bbd569d
AntonioSouls <tonyplanza1@gmail.com>
2025-05-05 21:29:45 +0200

    Aggiunto il codice per memorizzare lo stato dei forzieri

--- Assets/scripts/SaveController.cs
+++ Assets/scripts/SaveController.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System.Linq;
 
 public class SaveController : MonoBehaviour
 {
     private string saveLocation;
     private InventoryController inventoryController;
+    private Chest[] chests;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+    {      
+        InitializeComponents();
+        LoadGame();
+    }
+
+    private void InitializeComponents()
     {
         saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
         inventoryController = FindFirstObjectByType<InventoryController>();
-
-        LoadGame();
+        chests = FindObjectsByType<Chest>(FindObjectsSortMode.None);
     }
 
     public void SaveGame()
@@ -22,10 +29,27 @@ public class SaveController : MonoBehaviour
         SaveData saveData = new SaveData
         {
             playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position,
-            inventorySaveData = inventoryController.GetInventoryItems()
+            inventorySaveData = inventoryController.GetInventoryItems(),
+            chestSaveData = GetChestsState()
         };
 
         File.WriteAllText(saveLocation, JsonUtility.ToJson(saveData));
+    }
+
+    private List<ChestSaveData> GetChestsState()
+    {
+        List<ChestSaveData> chestStates = new List<ChestSaveData>();
+
+        foreach (Chest chest in chests)
+        {
+            ChestSaveData chestSaveData = new ChestSaveData
+            {
+                chestID = chest.ChestID,
+                isOpened = chest.IsOpened
+            };
+            chestStates.Add(chestSaveData);
+        }
+        return chestStates;
     }
 
     public void LoadGame()
@@ -37,11 +61,26 @@ public class SaveController : MonoBehaviour
             GameObject.FindGameObjectWithTag("Player").transform.position = saveData.playerPosition;
 
             inventoryController.SetInventoryItems(saveData.inventorySaveData);
+
+            LoadChestStates(saveData.chestSaveData);
         }
         else
         {
             inventoryController.SetInventoryItems(new List<InventorySaveData>());
             SaveGame();
+        }
+    }
+
+    private void LoadChestStates(List<ChestSaveData> chestStates)
+    {
+        foreach(Chest chest in chests)
+        {
+            ChestSaveData chestSaveData = chestStates.FirstOrDefault(c => c.chestID == chest.ChestID);
+
+            if(chestSaveData != null)
+            {
+                chest.SetOpened(chestSaveData.isOpened);
+            }
         }
     }
 
--- Assets/scripts/SaveData.cs
+++ Assets/scripts/SaveData.cs
@@ -7,4 +7,12 @@ public class SaveData
 {
     public Vector3 playerPosition;
     public List<InventorySaveData> inventorySaveData;
+    public List<ChestSaveData> chestSaveData;
+}
+
+[System.Serializable]
+public class ChestSaveData
+{
+    public string chestID;
+    public bool isOpened;
 }
