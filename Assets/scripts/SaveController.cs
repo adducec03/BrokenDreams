10eefc75c8d9582b93077bdace79c6aa31060db6
AntonioSouls <tonyplanza1@gmail.com>
2025-05-05 20:59:44 +0200

    Aggiunto un SaveSystem al gioco, sistemato l'intero inventario e creati i prefab di chiave e pozione. Adesso il comportamento delle casse funziona perfettamente. (Manca solo da memorizzare lo stato dei forzieri)

---
+++ Assets/scripts/SaveController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveController : MonoBehaviour
+{
+    private string saveLocation;
+    private InventoryController inventoryController;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
+        inventoryController = FindFirstObjectByType<InventoryController>();
+
+        LoadGame();
+    }
+
+    public void SaveGame()
+    {
+        SaveData saveData = new SaveData
+        {
+            playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position,
+            inventorySaveData = inventoryController.GetInventoryItems()
+        };
+
+        File.WriteAllText(saveLocation, JsonUtility.ToJson(saveData));
+    }
+
+    public void LoadGame()
+    {
+        if (File.Exists(saveLocation))
+        {
+            SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
+
+            GameObject.FindGameObjectWithTag("Player").transform.position = saveData.playerPosition;
+
+            inventoryController.SetInventoryItems(saveData.inventorySaveData);
+        }
+        else
+        {
+            inventoryController.SetInventoryItems(new List<InventorySaveData>());
+            SaveGame();
+        }
+    }
+
+}
